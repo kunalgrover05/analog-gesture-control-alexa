cd47abcc956d4f1fd9b66b62f60f76f0b6182651
Kunal Grover <kunalgrover05@gmail.com>
2020-09-19 21:31:29 +0530

    Cleanup code / directories

--- gesture_rec_body.py
+++ gesture_rec_body.py
@@ -1,11 +1,10 @@
-
 import cv2
 import time
 import numpy as np
 import threading
 from threading import Lock
-vcap = cv2.VideoCapture("rtsp://admin:test1234@192.168.1.14:554")
-#vcap = cv2.VideoCapture(0)
+#vcap = cv2.VideoCapture("rtsp://admin:test1234@192.168.1.14:554")
+vcap = cv2.VideoCapture(0)
 cv2.namedWindow('VIDEO', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('VIDEO', 600, 600)
 protoFile = "body/pose_deploy.prototxt.txt"
@@ -49,7 +48,7 @@ import queue
 
 q = queue.Queue()
 
-def worker():
+def http_queue_worker():
     print("Starting worker")
     while True:
         try:
@@ -75,143 +74,6 @@ def worker():
         except Exception as e:
             print("Failed", e)
 
-# turn-on the worker thread
-threading.Thread(target=worker, daemon=True).start()
-
-
-def getKeypoints(probMap, threshold=0.1):
-
-    mapSmooth = cv2.GaussianBlur(probMap,(3,3),0,0)
-
-    mapMask = np.uint8(mapSmooth>threshold)
-    keypoints = []
-
-    #find the blobs
-    contours, _ = cv2.findContours(mapMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    #for each blob find the maxima
-    for cnt in contours:
-        blobMask = np.zeros(mapMask.shape)
-        blobMask = cv2.fillConvexPoly(blobMask, cnt, 1)
-        maskedProbMap = mapSmooth * blobMask
-        _, maxVal, _, maxLoc = cv2.minMaxLoc(maskedProbMap)
-        keypoints.append(maxLoc + (probMap[maxLoc[1], maxLoc[0]],))
-
-    return keypoints
-
-
-volume = 0
-
-# Find valid connections between the different joints of a all persons present
-def getValidPairs(output):
-    valid_pairs = []
-    invalid_pairs = []
-    n_interp_samples = 10
-    paf_score_th = 0.1
-    conf_th = 0.7
-    # loop for every POSE_PAIR
-    for k in range(len(mapIdx)):
-        # A->B constitute a limb
-        pafA = output[0, mapIdx[k][0], :, :]
-        pafB = output[0, mapIdx[k][1], :, :]
-        pafA = cv2.resize(pafA, (frameWidth, frameHeight))
-        pafB = cv2.resize(pafB, (frameWidth, frameHeight))
-
-        # Find the keypoints for the first and second limb
-        candA = detected_keypoints[POSE_PAIRS[k][0]]
-        candB = detected_keypoints[POSE_PAIRS[k][1]]
-        nA = len(candA)
-        nB = len(candB)
-
-        # If keypoints for the joint-pair is detected
-        # check every joint in candA with every joint in candB
-        # Calculate the distance vector between the two joints
-        # Find the PAF values at a set of interpolated points between the joints
-        # Use the above formula to compute a score to mark the connection valid
-
-        if( nA != 0 and nB != 0):
-            valid_pair = np.zeros((0,3))
-            for i in range(nA):
-                max_j=-1
-                maxScore = -1
-                found = 0
-                for j in range(nB):
-                    # Find d_ij
-                    d_ij = np.subtract(candB[j][:2], candA[i][:2])
-                    norm = np.linalg.norm(d_ij)
-                    if norm:
-                        d_ij = d_ij / norm
-                    else:
-                        continue
-                    # Find p(u)
-                    interp_coord = list(zip(np.linspace(candA[i][0], candB[j][0], num=n_interp_samples),
-                                            np.linspace(candA[i][1], candB[j][1], num=n_interp_samples)))
-                    # Find L(p(u))
-                    paf_interp = []
-                    for k in range(len(interp_coord)):
-                        paf_interp.append([pafA[int(round(interp_coord[k][1])), int(round(interp_coord[k][0]))],
-                                           pafB[int(round(interp_coord[k][1])), int(round(interp_coord[k][0]))] ])
-                    # Find E
-                    paf_scores = np.dot(paf_interp, d_ij)
-                    avg_paf_score = sum(paf_scores)/len(paf_scores)
-
-                    # Check if the connection is valid
-                    # If the fraction of interpolated vectors aligned with PAF is higher then threshold -> Valid Pair
-                    if ( len(np.where(paf_scores > paf_score_th)[0]) / n_interp_samples ) > conf_th :
-                        if avg_paf_score > maxScore:
-                            max_j = j
-                            maxScore = avg_paf_score
-                            found = 1
-                # Append the connection to the list
-                if found:
-                    valid_pair = np.append(valid_pair, [[candA[i][3], candB[max_j][3], maxScore]], axis=0)
-
-            # Append the detected connections to the global list
-            valid_pairs.append(valid_pair)
-        else: # If no keypoints are detected
-            # print("No Connection : k = {}".format(k))
-            invalid_pairs.append(k)
-            valid_pairs.append([])
-    return valid_pairs, invalid_pairs
-
-
-
-# This function creates a list of keypoints belonging to each person
-# For each detected valid pair, it assigns the joint(s) to a person
-def getPersonwiseKeypoints(valid_pairs, invalid_pairs):
-    # the last number in each row is the overall score
-    personwiseKeypoints = -1 * np.ones((0, 19))
-
-    for k in range(len(mapIdx)):
-        if k not in invalid_pairs:
-            partAs = valid_pairs[k][:,0]
-            partBs = valid_pairs[k][:,1]
-            indexA, indexB = np.array(POSE_PAIRS[k])
-
-            for i in range(len(valid_pairs[k])):
-                found = 0
-                person_idx = -1
-                for j in range(len(personwiseKeypoints)):
-                    if personwiseKeypoints[j][indexA] == partAs[i]:
-                        person_idx = j
-                        found = 1
-                        break
-
-                if found:
-                    personwiseKeypoints[person_idx][indexB] = partBs[i]
-                    personwiseKeypoints[person_idx][-1] += keypoints_list[partBs[i].astype(int), 2] + valid_pairs[k][i][2]
-
-                # if find no partA in the subset, create a new subset
-                elif not found and k < 17:
-                    row = -1 * np.ones(19)
-                    row[indexA] = partAs[i]
-                    row[indexB] = partBs[i]
-                    # add the keypoint_scores for the two keypoints and the paf_score
-                    row[-1] = sum(keypoints_list[valid_pairs[k][i,:2].astype(int), 2]) + valid_pairs[k][i][2]
-                    personwiseKeypoints = np.vstack([personwiseKeypoints, row])
-    return personwiseKeypoints
-
-
 def rtsp_cam_buffer(vcap):
     global latest_frame, lo, last_ret
 
@@ -219,156 +81,62 @@ def rtsp_cam_buffer(vcap):
         with lo:
             last_ret, latest_frame = vcap.read()
 
+# HTTPRequests worker thread
+threading.Thread(target=http_queue_worker, daemon=True).start()
+# RTSP Buffer worker thread
+threading.Thread(target=rtsp_cam_buffer,args=(vcap,),name="rtsp_read_thread", daemon=True).start()
 
-# Get credentials for our API communication
-def get_credentials():
-    print("Logging in")
-    login_request = requests.post('https://api.sinric.pro/api/v1/auth', headers={
-
-        'Authorization': 'Basic ' + base64.b64encode(b'kunalgrover05@gmail.com:sinAdi#94').decode(),
-       # 'x-sinric-api-key': 'ba16848e-fc4b-43bd-95cb-78b698640dfd',
-        'Content-Type': 'application/x-www-form-urlencoded'},
-    data={'client_id': 'android_app'})
-    j = login_request.json()
-    print(login_request)
-    print(login_request.status_code)
-    return j['accessToken']
-
-from requests_threads import AsyncSession
-session = AsyncSession(n=10)
-# token = get_credentials()
-token = 1
-last_response = None
-
-def call_set_volume(token, delta):
-    global queue, last_response
-    #r = await session.get
-
-    queue.append(delta)
-
-    print("Setting volume")
-    # r = requests.get('https://api.sinric.pro/api/v1/devices/5f01b2ace7e680581e9089ab/action?action=setVolume&value={"volume": 25}&clientId=android_app&createdAt=1567852244&type=request',
-    #                headers={'Authorization': 'Bearer ' + token, 'Content-Type': 'application/x-www-form-urlencoded'})
-
-    if last_response is None:
-        print("sending new request")
-        last_response = requests.post('http://192.168.1.10/volume?delta=' + str(queue[-1]))
-        print(last_response)
-        last_response = None
+while(1):
+    start = time.time()
+    if (last_ret is not None) and (latest_frame is not None):
+        image = latest_frame.copy()
     else:
-        print("Still running previous request")
+        time.sleep(0.01)
+        continue
 
-t1 = threading.Thread(target=rtsp_cam_buffer,args=(vcap,),name="rtsp_read_thread")
-t1.daemon=True
-t1.start()
+    frameWidth = image.shape[1]
+    frameHeight = image.shape[0]
+    print(frameWidth, frameHeight)
 
-with open(str(datetime.datetime.now()), 'w', newline='') as file:
-    writer = csv.writer(file)
-    while(1):
-        if (last_ret is not None) and (latest_frame is not None):
-            image1 = latest_frame.copy()
-        else:
-            time.sleep(0.1)
-            continue
+    net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
+
+    # Fix the input Height and get the width according to the Aspect Ratio
+    inHeight = 100
+    inWidth = int((inHeight/frameHeight)*frameWidth)
+
+    # Create a resized image for processing
+    image = cv2.resize(image, (int((480/frameHeight)*frameWidth), 480))
+    
+    inpBlob = cv2.dnn.blobFromImage(image, 1.0 / 255, (inWidth, inHeight),
+                              (0, 0, 0), swapRB=False, crop=False)
+    net.setInput(inpBlob)
+    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA);
+    net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA);
+    output = net.forward()
+
+    print("Time Taken in forward pass = {}".format(time.time() - start))
+
+    BODY_PARTS = { "RShoulder": 2, "RWrist": 4, "LShoulder": 5 }
 
 
-        frameWidth = image1.shape[1]
-        frameHeight = image1.shape[0]
-        print(frameWidth, frameHeight)
-        
+    points = {}
+    for part, i in BODY_PARTS.items():
+        # Slice heatmap of corresponding body's part.
+        heatMap = output[0, i, :, :]
 
-        t = time.time()
-        net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
+        # Originally, we try to find all the local maximums. To simplify a sample
+        # we just find a global one. However only a single pose at the same time
+        # could be detected this way.
+        _, conf, _, point = cv2.minMaxLoc(heatMap)
+        x = (frameWidth * point[0]) / output.shape[3]
+        y = (frameHeight * point[1]) / output.shape[2]
 
-        # Fix the input Height and get the width according to the Aspect Ratio
-        inHeight = 100
-        inWidth = int((inHeight/frameHeight)*frameWidth)
+        # Add a point if it's confidence is higher than threshold.
+        location = ((int(x), int(y)) if conf > 0.1 else None)
+        points[part] = location
+        cv2.circle(image, location, 5, colors[BODY_PARTS[part]%len(colors)], -1, cv2.LINE_AA)
 
-        # Create a resized image for processing
-        image1 = cv2.resize(image1, (int((480/frameHeight)*frameWidth), 480))
-        
-        # Show the latest frame post resize
-        cv2.imshow("Frame" , image1)
-        cv2.waitKey(1)
-        
-        inpBlob = cv2.dnn.blobFromImage(image1, 1.0 / 255, (inWidth, inHeight),
-                                  (0, 0, 0), swapRB=False, crop=False)
-
-        net.setInput(inpBlob)
-        net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA);
-        net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA);
-        output = net.forward()
-        print("Time Taken in forward pass = {}".format(time.time() - t))
-
-        detected_keypoints = []
-        keypoints_list = np.zeros((0,3))
-        keypoint_id = 0
-        threshold = 0.1
-
-        # Get keypoint mapping with part array
-        detected_parts = {}
-        for part in range(nPoints):
-            probMap = output[0,part,:,:]
-            probMap = cv2.resize(probMap, (image1.shape[1], image1.shape[0]))
-            keypoints = getKeypoints(probMap, threshold)
-            # print("Keypoints - {} : {}".format(keypointsMapping[part], keypoints))
-            keypoints_with_id = []
-            for i in range(len(keypoints)):
-                keypoints_with_id.append(keypoints[i] + (keypoint_id,))
-                keypoints_list = np.vstack([keypoints_list, keypoints[i]])
-                keypoint_id += 1
-
-            detected_keypoints.append(keypoints_with_id)
-            detected_parts[keypointsMapping[part]] = keypoints
-
-        if len(detected_parts['R-Sho']) == 1 and len(detected_parts['L-Sho']) == 1 and len(detected_parts['R-Wr']) == 1:
-            # Find distance from L and R sho and find approx position
-            
-            d1 = (np.linalg.norm(np.array(detected_parts['R-Sho'])-np.array(detected_parts['R-Wr'])))
-            d2 = (np.linalg.norm(np.array(detected_parts['L-Sho'])-np.array(detected_parts['R-Wr'])))
-
-            # If the R-Wr is outside the bounds, then we are good with what we already have.
-            if detected_parts['R-Wr'][0][0] < detected_parts['R-Sho'][0][0]:
-                result = 5
-            elif detected_parts['R-Wr'][0][0] > detected_parts['L-Sho'][0][0]:
-                result = -5
-            else:
-                result = 5 + int(-10*d1/ (d1+d2))
-
-            print("R-Sho", detected_parts['R-Sho'])
-            print("L-Sho", detected_parts['L-Sho'])
-            print("R-Wr", detected_parts['R-Wr'])
-            print("Result", result)
-            
-            #engine.say("Setting volume to"+str(result))
-            #engine.runAndWait()
-
-            # session.run(call_set_volume)
-            q.put({'value':result, 'time': time.time()})
-            #callThread = threading.Thread(target=call_set_volume, args=(token, result), name="call_service")
-            #callThread.start()
-
-#        frameClone = image1.copy()
-#        for i in range(nPoints):
-#            for j in range(len(detected_keypoints[i])):
-#                cv2.circle(frameClone, detected_keypoints[i][j][0:2], 5, colors[i], -1, cv2.LINE_AA)
-#        cv2.imshow("Keypoints",frameClone)
-#
-#        valid_pairs, invalid_pairs = getValidPairs(output)
-#        personwiseKeypoints = getPersonwiseKeypoints(valid_pairs, invalid_pairs)
-#
-#        for i in range(17):
-#            for n in range(len(personwiseKeypoints)):
-#                index = personwiseKeypoints[n][np.array(POSE_PAIRS[i])]
-#                if -1 in index:
-#                    continue
-#                B = np.int32(keypoints_list[index.astype(int), 0])
-#                A = np.int32(keypoints_list[index.astype(int), 1])
-#                cv2.line(frameClone, (B[0], A[0]), (B[1], A[1]), colors[i], 3, cv2.LINE_AA)
-#
-#
-#        cv2.imshow("Detected Pose" , frameClone)
-#        cv2.waitKey(1)
-#
-        print("Total Time Taken in code = {}".format(time.time() - t))
+    cv2.imshow("Keypoints", image)
+    cv2.waitKey(1)
+    print("Total Time Taken in code = {}".format(time.time() - start))
 
